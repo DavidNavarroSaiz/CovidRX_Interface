e9dce5800bdba203fcbf0df5df62439df0165273
David C Navarro <32474368+DavidNavarroSaiz@users.noreply.github.com>
2021-05-17 22:23:46 -0500

    load image working

--- scr/views/icon.py
+++
@@ -1,9 +0,0 @@
-from PySide2.QtWidgets impor QApplication, QWidget
-import sys
-from pyside2.QtGui impor QIcon
-
-class Window(QWidget):
-    def __init__(self):
-        super().__init__()
-
-        self.
